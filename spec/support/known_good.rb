cdd3468bb43f99abbbc14e2710d0e77cca094cec
Matias Korhonen <korhonen.matt@gmail.com>
2010-09-06 19:20:16 +0300

    Workaround for JRuby bug JRUBY-5064

--- spec/support/known_good.rb
+++ spec/support/known_good.rb
@@ -1,5 +1,11 @@
 # -*- encoding: utf-8 -*-
 module KnownGood
-  ENCODE62 = Marshal.load(File.open("./spec/support/encode62_examples", "r"))
-  DECODE62 = Marshal.load(File.open("./spec/support/decode62_examples", "r"))
+   # Workaround for JRuby bug JRUBY-5064 (http://jira.codehaus.org/browse/JRUBY-5064)
+  if defined?(JRUBY_VERSION)
+    ENCODE62 = Marshal.load(File.read("./spec/support/encode62_examples"))
+    DECODE62 = Marshal.load(File.read("./spec/support/decode62_examples"))
+  else
+    ENCODE62 = Marshal.load(File.open("./spec/support/encode62_examples", "r"))
+    DECODE62 = Marshal.load(File.open("./spec/support/decode62_examples", "r"))
+  end
 end
